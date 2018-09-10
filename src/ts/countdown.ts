8eba005532f1df82bbafebac85f36e18ee1e540f
ooyama <ooyama@geek.co.jp>
2018-09-10 14:57:09 +0900

    fix #3 Issue

--- src/ts/countdown.ts
+++ src/ts/countdown.ts
@@ -18,26 +18,19 @@ class CountdownTimer {
         const sec = Math.floor(((this.tl - Number(today)) % (24 * 60 * 60 * 1000)) / 1000) % 60 % 60;
         if ((this.tl - Number(today)) > 0 || (this.tl - Number(today)) < -86400000) {
             let timer = "";
-            timer += `
-            <p>やまのくの誕生日まで</p>
-            <div class="number">
-                <p><span class="number-day">${day}</span><span class="number_value">day</span></p>
-                <p><span class="number-hour">${this.addZero(hour)}</span><span class="number_value">hour</span></p>
-                <p><span class="number-min">${this.addZero(min)}</span><span class="number_value">min</span></p>
-                <p><span class="number-sec">${this.addZero(sec)}</span><span class="number_value">sec</span></p>
-            </div>
-            `;
+            timer += `<p>やまのくの誕生日まで</p><div class="number">
+            <p><span class="number-day">${day}</span><span class="number_value">day</span></p>
+            <p><span class="number-hour">${this.addZero(hour)}</span><span class="number_value">hour</span></p>
+            <p><span class="number-min">${this.addZero(min)}</span><span class="number_value">min</span></p>
+            <p><span class="number-sec">${this.addZero(sec)}</span><span class="number_value">sec</span></p>
+            </div>`;
             document.getElementById("CDT").innerHTML = timer;
             setTimeout(() => {
                 this.countDown();
             }, 10);
         } else {
             let mes = "";
-            mes += `
-            <p class="birth_end">今日誕生日です</p>
-            <a class="birth_btn" href="http://amzn.asia/cti4d0v" target="_blank">欲しいものを送ってやる</a>
-            <a class="birth_btn" href="http://amzn.asia/8Kh4dGA" target="_blank">酒を送ってやる</a>
-            `;
+            mes += '<p class="birth_end">今日誕生日です</p><a class="birth_btn" href="http://amzn.asia/cti4d0v" target="_blank">欲しいものを送ってやる</a><a class="birth_btn" href="http://amzn.asia/8Kh4dGA" target="_blank">酒を送ってやる</a>';
             document.getElementById("CDT").innerHTML = mes;
             return;
         }
