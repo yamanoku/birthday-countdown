9792b91ccc379794e08599ed77bd61ce0c2453d0
yamanoku <0910yama@gmail.com>
2022-03-26 12:03:40 +0900

    update: fileNames

--- vite.config.js
+++ vite.config.js
@@ -2,4 +2,13 @@ import { defineConfig } from 'vite'
 
 export default defineConfig({
   base: '/birthday-countdown-js/',
+  build: {
+    rollupOptions: {
+      output: {
+        entryFileNames: `assets/[name].js`,
+        chunkFileNames: `assets/[name].js`,
+        assetFileNames: `assets/[name].[ext]`
+      }
+    }
+  }
 })
