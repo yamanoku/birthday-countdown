8adc2fa72764702b759fc538af12020bb0db3466
yamanoku <0910yama@gmail.com>
2023-07-08 10:24:33 +0900

    del: eslint-prettier-plugin

--- .eslintrc.js
+++ .eslintrc.js
@@ -4,7 +4,6 @@ module.exports = {
     },
     "extends": [
         "plugin:@typescript-eslint/recommended",
-        "plugin:prettier/recommended"
     ],
     "plugins": [
         "@typescript-eslint"
