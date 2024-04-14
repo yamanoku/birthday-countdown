78ac0e61fb00c1da9d5ab45cbb2cb1ff87d8cbd5
yamanoku <0910yama@gmail.com>
2024-04-14 21:00:04 +0900

    del viewport

--- app/routes/_renderer.tsx
+++ app/routes/_renderer.tsx
@@ -8,7 +8,6 @@ export default jsxRenderer(({ children, title }) => {
     <html class={htmlClass} lang="ja">
       <head>
         <meta charset="utf-8" />
-        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
         <title>{title}</title>
         <meta name="description" content="countdown @yamanoku birthday limit" />
         <meta name="image" content="https://yamanoku.net/og-images/birthday-countdown.png" />
