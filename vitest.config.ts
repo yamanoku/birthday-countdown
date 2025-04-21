31f832d552064bbbb662522492fe957e88679610
yamanoku <0910yama@gmail.com>
2025-04-21 18:12:29 +0900

    biome

--- vitest.config.ts
+++ vitest.config.ts
@@ -1,12 +1,12 @@
-import { defineConfig } from 'vitest/config';
+import { defineConfig } from "vitest/config";
 
 export default defineConfig({
   test: {
     globals: true,
-    environment: 'node',
-    includeSource: ['app/**/*.ts'],
+    environment: "node",
+    includeSource: ["app/**/*.ts"]
   },
   define: {
-    'import.meta.vitest': 'undefined',
-  },
+    "import.meta.vitest": "undefined"
+  }
 });
