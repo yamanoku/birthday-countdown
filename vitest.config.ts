432f18e1ee9ece6c61521b2134dfa41c539e86c5
yamanoku <0910yama@gmail.com>
2025-04-21 18:11:21 +0900

    add: vitest

---
+++ vitest.config.ts
@@ -0,0 +1,12 @@
+import { defineConfig } from 'vitest/config';
+
+export default defineConfig({
+  test: {
+    globals: true,
+    environment: 'node',
+    includeSource: ['app/**/*.ts'],
+  },
+  define: {
+    'import.meta.vitest': 'undefined',
+  },
+});
